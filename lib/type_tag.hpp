cbee1404dcb141d0aa5bb7569fd73dda3e743373
Bronek Kozicki <brok@spamcop.net>
2018-02-03 22:17:34 +0000

    some refactoring

--- lib/type_tag.hpp
+++ lib/type_tag.hpp
@@ -9,24 +9,26 @@
 #include <cstdint>
 
 namespace juno {
-    template<typename T> struct type_tag;
+    namespace d {
+        template<typename T> struct type_tag {
+            using type = T;
+            constexpr static char tag_ = 0;
+            const static uintptr_t value;
+        };
 
-    template<typename T> class type_tag_impl {
-        template<typename U> friend struct type_tag;
+        template<typename T>
+        constexpr char type_tag<T>::tag_;
 
-        constexpr static char tag_ = 0;
-        const static uintptr_t value;
-    };
+        template<typename T>
+        const uintptr_t type_tag<T>::value = reinterpret_cast<uintptr_t>(&tag_);
+    }
 
-    template<typename T>
-    constexpr char type_tag_impl<T>::tag_;
+    template<typename T> class type_tag
+            : private d::type_tag<typename std::remove_cv<typename std::remove_reference<T>::type>::type> {
+        using impl = d::type_tag<typename std::remove_cv<typename std::remove_reference<T>::type>::type>;
 
-    template<typename T>
-    const uintptr_t type_tag_impl<T>::value = reinterpret_cast<uintptr_t>(&tag_);
-
-    template<typename T> struct type_tag
-            : private type_tag_impl<typename std::remove_cv<typename std::remove_reference<T>::type>::type> {
-        using type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
-        using type_tag_impl<type>::value;
+    public:
+        using type = typename impl::type;
+        using impl::value;
     };
 }
