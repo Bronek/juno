0a437faa1ac973bbb29fc19ed98121fe8ee32506
Bronek Kozicki <brok@spamcop.net>
2018-02-04 15:48:03 +0000

    renamed license file

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Bronislaw (Bronek) Kozicki
 //
-// Distributed under the MIT License. See accompanying file license.txt
+// Distributed under the MIT License. See accompanying file LICENSE
 // or copy at https://opensource.org/licenses/MIT
 
 #include <cstdio>
