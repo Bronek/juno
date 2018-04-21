be80c2781ae160a01630121d05d4c5f341050a82
Bronek Kozicki <brok@spamcop.net>
2018-04-21 21:47:34 +0100

    started work on map

--- app/main.cpp
+++ app/main.cpp
@@ -3,14 +3,31 @@
 // Distributed under the MIT License. See accompanying file LICENSE
 // or copy at https://opensource.org/licenses/MIT
 
-#include <cstdio>
+#include <iostream>
+#include <string>
 
 #include <mpl.hpp>
 #include <set.hpp>
+#include <map.hpp>
 #include <tagset.hpp>
 #include <cassert>
 
 struct Foo; struct Bar; struct Fuz;
+
+template <typename T, typename Map>
+void print(const Map& map) {
+    std::cout << map.template get<T>() << std::endl;
+}
+
+struct NoCopy{
+    const int val;
+    NoCopy(int v) : val(v) {}
+    NoCopy(const NoCopy&) = delete;
+};
+
+std::ostream& operator<< (std::ostream& s, const NoCopy& c) {
+    return (s << c.val);
+}
 
 int main() {
     using namespace juno;
@@ -34,6 +51,22 @@ int main() {
     assert((ts1.intersects<int, Foo>()));
     assert(not ts1.intersects<>());
 
-    std::puts("Ta-Da!");
-    return 0;
+    const auto m2 = map<>().add<Foo>(0).add<Bar>("Hello");
+    print<Foo>(m2);
+    print<Bar>(m2);
+    const auto m3 = m2.add<Fuz>(std::string("World"));
+    print<Fuz>(m3);
+
+    const auto mm = map<>().add<Foo>([] (const auto& a) {
+        std::cout << "Hello " << a << std::endl;
+    }).add<Bar>([] (const auto& a) {
+        std::cout << "Bye " << a << "!" << std::endl;
+    }).add<Fuz>([&i] (int a) {
+        return NoCopy(a * i);
+    }).add<int>(42);
+    mm.template run<Foo>("There");
+    i = 3;
+    const auto n = mm.template run<Fuz>(14);
+    mm.template run<Bar>(n);
+    mm.template get<Bar>()(mm.template get<int>());
 }
---
+++ lib/map.hpp
@@ -0,0 +1,101 @@
+// Copyright (c) 2017-2018 Bronislaw (Bronek) Kozicki
+//
+// Distributed under the MIT License. See accompanying file LICENSE
+// or copy at https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "mpl.hpp"
+#include "set.hpp"
+
+#include <type_traits>
+
+namespace juno {
+    template <typename ...L> struct map;
+
+    namespace impl_map {
+        template <typename ...L> struct impl;
+
+        template <>
+        struct impl<> {
+            using set = juno::set<>;
+        };
+
+        template <typename TagT> struct wrap {};
+
+        template <typename MyT, typename MyV, typename ...L>
+        struct impl<MyT, MyV, L...> : impl<L...> {
+            static_assert(std::is_same<MyT,
+                    typename std::remove_cv<typename std::remove_reference<MyT>::type>::type>::value);
+            static_assert(not std::is_same<MyT, void>::value);
+            static_assert(not std::is_same<MyV, void>::value);
+            static_assert(not impl<L...>::set::template contains<MyT>);
+
+            using set = typename juno::set<MyT>::template insert_set<typename impl<L...>::set>::type;
+
+            explicit impl(MyV&& v) : impl<L...>() , value(v)
+            {}
+
+            explicit impl(MyV&& v, const impl<L...>& p)
+                : impl<L...>(p)
+                , value(v)
+            {}
+
+            const auto& get(wrap<MyT>) const {
+                return value;
+            }
+
+            template <typename TagT>
+            const auto& get(wrap<TagT> t) const {
+                return impl<L...>::get(t);
+            }
+
+        private:
+            MyV value;
+        };
+    }
+
+    template <typename ...L> struct map;
+
+    template <> struct map<> {
+        using set = juno::set<>;
+
+        template <typename TagT, typename ValT>
+        auto add(ValT&& val) const {
+            using result = map<
+                    typename std::remove_cv<typename std::remove_reference<TagT>::type>::type, ValT>;
+            return result(typename result::impl(std::forward<ValT>(val)));
+        }
+    };
+
+    template <typename MyT, typename MyV, typename ...L> struct map<MyT, MyV, L...> {
+    private:
+        template <typename ...P> friend struct map;
+
+        using impl = impl_map::impl<MyT, MyV, L...>;
+        impl impl_;
+
+        explicit map(impl&& i) : impl_(i) {}
+
+    public:
+        using set = typename impl::set;
+
+        template <typename TagT, typename ValT>
+        auto add(ValT&& val) const {
+            using result = map<typename std::remove_cv<typename std::remove_reference<TagT>::type>::type,
+                    ValT, MyT, MyV, L...>;
+            return result(typename result::impl(std::forward<ValT>(val), impl_));
+        }
+
+        template <typename TagT>
+        const auto& get() const {
+            using T = typename std::remove_cv<typename std::remove_reference<TagT>::type>::type;
+            return impl_.template get(impl_map::wrap<T>());
+        }
+
+        template <typename TagT, typename ...P>
+        auto run(P&& ... p) const {
+            return this->template get<TagT>()(std::forward<P>(p)...);
+        }
+    };
+}
