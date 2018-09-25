6fd42c0e98f4b1a80dc78a5d9479da48093eeb31
Bronek Kozicki <brok@spamcop.net>
2018-09-25 13:45:54 +0100

    clean slate

--- lib/map.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2017 Bronislaw (Bronek) Kozicki
-//
-// Distributed under the MIT License. See accompanying file LICENSE
-// or copy at https://opensource.org/licenses/MIT
-
-#include "map.hpp"
